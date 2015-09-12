c2d7359e79494a5a8f7d3b5777f992702f456536
jingri <jingr-1@163.com>
2015-09-12 11:04:03 +0800

    FIRST REV

--- Makefile
+++ Makefile
@@ -1,1 +1,11 @@
+test.bin: start.o main.o
+	arm-linux-ld -T fortest.lds -o test.elf start.o main.o
+	arm-linux-objcopy -O binary test.elf test.bin
+	arm-linux-objdump -D test.elf > test.dis
+start.o: start.S
+	arm-linux-gcc -o start.o start.S -c
+main.o: main.c
+	arm-linux-gcc -o main.o main.c -c
+clean:
+	rm *.o test.*
 
--- main.c
+++ main.c
@@ -1,1 +1,19 @@
+#define GPMCON *((volatile unsigned long*)0x7F008820)
+#define GPMDAT *((volatile unsigned long*)0x7F008824)
+#define GPMPUD *((volatile unsigned long*)0x7F008828)
 
+int main()
+{
+	static int flag =12;
+	GPMCON = (GPMCON & ~0xffff)|0x1111;
+	GPMPUD = (GPMPUD & ~0xff)|0x55;
+	GPMDAT = (GPMDAT & ~0x0f)|0x0f;
+	if (12 == flag)
+	{
+		GPMDAT = 0x00;
+	}
+	else
+		GPMDAT = 0x0f;
+	while(1);
+	return 0;
+}
